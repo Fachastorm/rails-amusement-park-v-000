ecf0fa1c1299243d588c406469df61574df5c66b
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-01-17 07:02:54 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-
+    
+root 'application#home' 
+get '/signin' => 'sessions#new'
+resources :sessions, only: [:create, :destroy]
+resources :rides, only:[:create]
+resources :users, :attractions
 
 end
